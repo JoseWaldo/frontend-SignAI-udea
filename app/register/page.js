27a521bcea4d8873ee858e7ae9c989bbed9ff471
Jose Waldo <josferw@gmail.com>
2023-06-30 16:06:33 -0500

    Feat: add the structure for the register page

--- app/register/page.js
+++ app/register/page.js
@@ -1,7 +1,20 @@
+import stylesLogin from "@/styles/loginPage.module.css";
+import FormRegister from "@/components/form-register";
+
 function Page() {
   return (
     <>
-      <h1>REGISTER</h1>
+      <main className={stylesLogin.wrapperLogin}>
+        <figure className={stylesLogin.wrapperImage}>
+          <img
+            className={stylesLogin.imgHeader}
+            src="/images/logo-udea.png"
+            alt="Logo de la Universidad de Antioquia"
+            loading="lazy"
+          />
+        </figure>
+        <FormRegister />
+      </main>
     </>
   );
 }
