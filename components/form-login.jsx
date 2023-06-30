3055e1403da5f1058ed695bf2436f291d42a1fe4
Jose Waldo <josferw@gmail.com>
2023-06-30 12:30:23 -0500

    Feat: add a login form component

---
+++ components/form-login.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import styles from "@/styles/formLogin.module.css"
+
+function FormLogin() {
+ return ( 
+  <form className={styles.form}>
+   <h2 className={styles.titleForm}>SignAI</h2>
+   <label htmlFor="usernameInput">
+    <span className={styles.labelForm}>Usuario</span>
+   </label>
+   <input className={styles.inputForm} type="text" id="usernameInput"/>
+   <label htmlFor="passwordInput">
+    <span className={styles.labelForm}>Contraseña</span>
+    </label>
+   <input className={styles.inputForm} type="password" id="passwordInput"/>
+   <button className={styles.button} type="submit">Ingresar</button>
+   <Link className={styles.link} href="#">Registrarse</Link>
+  </form>
+ );
+}
+
+export default FormLogin;
