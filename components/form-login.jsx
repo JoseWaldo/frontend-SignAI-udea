ee01e66cd0005effd42f86b33397b9d85fad9286
Jose Waldo <josferw@gmail.com>
2023-06-30 16:46:32 -0500

    Feat: add the form controller

--- components/form-login.jsx
+++ components/form-login.jsx
@@ -1,18 +1,47 @@
+"use client"
+
 import Link from "next/link";
 import styles from "@/styles/formLogin.module.css"
+import { useState } from "react";
 
 function FormLogin() {
+
+ const [inputs, setInputs] = useState({
+  username: "",
+  password: "",
+ });
+
+ const handleChange = (evt) => {
+  const { target } = evt;
+  const { name, value } = target;
+
+  const newValues = {
+   ...inputs,
+   [name]: value,
+  };
+
+  setInputs(newValues);
+ }
+
+ const handleSubmit = (evt) => {
+
+  evt.preventDefault();
+
+  console.log("Username: " + inputs.username);
+  console.log("Password: " + inputs.password);
+ }
+
  return ( 
-  <form className={styles.form}>
+  <form className={styles.form} onSubmit={handleSubmit}>
    <h2 className={styles.titleForm}>SignAI</h2>
    <label htmlFor="usernameInput">
     <span className={styles.labelForm}>Usuario</span>
    </label>
-   <input className={styles.inputForm} type="text" id="usernameInput"/>
+   <input className={styles.inputForm} type="text" id="usernameInput" name="username" onChange={handleChange}/>
    <label htmlFor="passwordInput">
     <span className={styles.labelForm}>Contraseña</span>
     </label>
-   <input className={styles.inputForm} type="password" id="passwordInput"/>
+   <input className={styles.inputForm} type="password" id="passwordInput" name="password" onChange={handleChange}/>
    <button className={styles.button} type="submit">Ingresar</button>
    <Link className={styles.link} href="/register">Registrarse</Link>
   </form>
